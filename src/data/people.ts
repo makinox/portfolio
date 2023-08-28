da02cc790ca209a0de593358f6c90d35551c755d
Jesus Bossa <x@jesusbossa.dev>
2023-08-28 10:21:49 -0500

    refactor: change people image sources

--- src/data/people.ts
+++ src/data/people.ts
@@ -1,19 +1,19 @@
 export default [
   {
     name: 'Alessandra Portugal',
-    src: 'https://jesusbossa.dev/static/80843fda60ec6c3566acb41369ec472f/6d0de/people2.avif',
+    src: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/people/people2.jpeg',
     place: 'Crehana',
     text: 'Three words to describe Jesus: innovative, accountable, and multifaceted. During our time working together, he showed those 3 characteristics, while we were developing a Desktop App, embracing uncertainty and finding solutions for a timely delivery. Thanks to that we managed to launch it successfully to the market, learned to use new technologies, and set the basis for other similar projects. He was also constantly researching and experimenting with new technologies and design tools. I would recommend him to any company that is not afraid of exploring the unknown and challenging the status quo!',
   },
   {
     name: 'Fernando Antonio Llinás Giraldo',
-    src: 'https://jesusbossa.dev/static/3ab4df86bfe8aebb25cdb105cdd7412d/6d0de/people1.avif',
+    src: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/people/people1.jpeg',
     place: 'SOLUNTECH',
     text: 'Innovation and learning is his personal hallmark. He is always willing to give more than 100%. As a professional, he is open to feedback and taking on new challenges.  If you hired him, you will won not only a great asset, also astonishing human being.',
   },
   {
     name: 'Fernando Florez',
-    src: 'https://jesusbossa.dev/static/23d5150a7f3fc12f9852e7aceae5781e/6d0de/people3.avif',
+    src: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/people/people3.jpeg',
     place: 'iSofact',
     text: 'It was something exciting to work with Jesus. He was a good lead and he was always willing to help me when I needed. He has a significant experience in Frontend, experience that allowed us to create great products.',
   },
