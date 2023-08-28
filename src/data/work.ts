234547973def7a0dd637863bea3259cb273e8544
Jesus Bossa <x@jesusbossa.dev>
2023-08-28 10:17:52 -0500

    refactor: change image sources

--- src/data/work.ts
+++ src/data/work.ts
@@ -4,7 +4,7 @@ export default [
     place: 'Eva',
     timeCount: 'Currently since March 2022',
     link: 'https://evacenter.com/',
-    image: 'https://jesusbossa.dev/static/79293813a8aef9e4cacd7ab5c366bfc6/6d0de/work1.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work1.avif',
   },
   {
     job: 'Product Engineer',
@@ -18,21 +18,21 @@ export default [
     place: 'Crehana',
     timeCount: '1 year and 1 month',
     link: 'https://www.crehana.com/',
-    image: 'https://jesusbossa.dev/static/e10c8eea7de71581369205643c394d80/6d0de/work2.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work2.avif',
   },
   {
     job: 'Front End Developer',
     place: 'Soluntech',
     timeCount: '9 months',
     link: 'https://soluntech.com/',
-    image: 'https://jesusbossa.dev/static/5c1af808511a7f41142d2f7c90444e82/6d0de/work3.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work3.avif',
   },
   {
     job: 'Full Stack Developer',
     place: 'iSofact',
     timeCount: '2 years and 6 months',
     link: 'https://www.isofact.com/',
-    image: 'https://jesusbossa.dev/static/8b5858200791fb362c968609fb8bfabb/6d0de/work4.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work4.avif',
   },
 
   {
@@ -40,20 +40,20 @@ export default [
     place: 'Jezsy',
     timeCount: '1 year and 1 month',
     link: 'https://www.linkedin.com/in/makinox/details/experience/',
-    image: 'https://jesusbossa.dev/static/3e6d1f5b1658cca2f19b5a1bef7220e8/f2685/work5.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work5.avif',
   },
   {
     job: 'Software Developer Lead',
     place: 'RegyPOS',
     timeCount: '4 months',
     link: 'https://empresite.eleconomistaamerica.co/REGISTRADORAS-EQUIPOS-CARIBE-SAS.html',
-    image: 'https://jesusbossa.dev/static/430417f247752210aaab15ed1664ed19/6d0de/work6.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work6.avif',
   },
   {
     job: 'System assistant engineer',
     place: 'GRUPO COLBA',
     timeCount: '6 months',
     link: 'https://www.grupocolba.com.co/',
-    image: 'https://jesusbossa.dev/static/5ff431ca70ebd64b067f73a760d87ced/6d0de/work7.avif',
+    image: 'https://buck-one.s3.us-south.cloud-object-storage.appdomain.cloud/portfolioImages/work/work7.avif',
   },
 ];
